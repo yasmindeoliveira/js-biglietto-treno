d726685eb52068310bae0459543f2045cf0d9559
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-11-10 15:41:54 +0100

    calcolo sconto prezzo biglietto

--- script.js
+++ script.js
@@ -6,4 +6,17 @@ console.log("Età passegiero: " + eta);
 
 parseInt(chilometri);
 let prezzoBiglietto = 0.21 * chilometri;
-console.log("Prezzo del Biglietto: " + prezzoBiglietto + "€");
+let prezzoBigliettoIntero = Math.floor(prezzoBiglietto);
+console.log("Prezzo del Biglietto: " + prezzoBigliettoIntero + "€");
+
+if (eta < 18){
+    let scontoMinorenne = prezzoBiglietto * 20 / 100;
+    let scontoMinorenneTotale = prezzoBigliettoIntero - scontoMinorenne;
+    let scontoMinorenneInteroTotale = Math.floor(scontoMinorenneTotale);
+    console.log("Totale scontato per minorenni: " + scontoMinorenneInteroTotale + "€");
+} else if (eta >= 65){
+    let scontoSenior = prezzoBiglietto * 40 / 100;
+    let scontoSeniorTotale = prezzoBigliettoIntero - scontoSenior;
+    let scontoSeniorInteroTotale = Math.floor(scontoSeniorTotale);
+    console.log("Totale scontato per seniors: " + scontoSeniorInteroTotale + "€");
+};
