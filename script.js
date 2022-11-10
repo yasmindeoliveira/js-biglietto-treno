dd84d7c16970df02105fd6bd0eeea6f961fb5cd2
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-11-10 16:01:18 +0100

    limite detà minorenni

--- script.js
+++ script.js
@@ -4,19 +4,26 @@ let eta = prompt("Inserisca la sua età:");
 console.log("Chilometri da percorrere: " + chilometri + "km");
 console.log("Età passegiero: " + eta);
 
-parseInt(chilometri);
-let prezzoBiglietto = 0.21 * chilometri;
-let prezzoBigliettoIntero = Math.floor(prezzoBiglietto);
-console.log("Prezzo del Biglietto: " + prezzoBigliettoIntero + "€");
+if (eta < 0) {
+    console.log("Scusaci, l'operazione non può essere eseguita. Perfavore inserire un'età seperiore a 0");
+} else {
 
-if (eta < 18){
-    let scontoMinorenne = prezzoBiglietto * 20 / 100;
-    let scontoMinorenneTotale = prezzoBigliettoIntero - scontoMinorenne;
-    let scontoMinorenneInteroTotale = Math.floor(scontoMinorenneTotale);
-    console.log("Totale scontato per minorenni: " + scontoMinorenneInteroTotale + "€");
-} else if (eta >= 65){
-    let scontoSenior = prezzoBiglietto * 40 / 100;
-    let scontoSeniorTotale = prezzoBigliettoIntero - scontoSenior;
-    let scontoSeniorInteroTotale = Math.floor(scontoSeniorTotale);
-    console.log("Totale scontato per seniors: " + scontoSeniorInteroTotale + "€");
+    parseInt(chilometri);
+    let prezzoBiglietto = 0.21 * chilometri;
+    let prezzoBigliettoIntero = Math.floor(prezzoBiglietto);
+    console.log("Prezzo totale del Biglietto: " + prezzoBigliettoIntero + "€");
+
+    if (eta < 18) {
+        let scontoMinorenne = prezzoBiglietto * 20 / 100;
+        let scontoMinorenneTotale = prezzoBigliettoIntero - scontoMinorenne;
+        let scontoMinorenneInteroTotale = Math.floor(scontoMinorenneTotale);
+        console.log("Totale scontato per minorenni: " + scontoMinorenneInteroTotale + "€");
+    } else if (eta >= 65) {
+        let scontoSenior = prezzoBiglietto * 40 / 100;
+        let scontoSeniorTotale = prezzoBigliettoIntero - scontoSenior;
+        let scontoSeniorInteroTotale = Math.floor(scontoSeniorTotale);
+        console.log("Totale scontato per seniors: " + scontoSeniorInteroTotale + "€");
+    };
+
 };
+
